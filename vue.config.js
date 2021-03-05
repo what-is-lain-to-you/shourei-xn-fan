1245fc7667ebe34a5e37e8cce2dde7c9c8366bca
What-is-lain-to-you？ <58593583+what-is-lain-to-you@users.noreply.github.com>
2021-03-05 19:46:54 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     ],
 
     pwa: {
-      name: 'ぺこらボタン',
+      name: 'XN-Button',
       msTileColor: '#728bf3',
       appleMobileWebAppCapable: 'yes',
       appleMobileWebAppStatusBarStyle: 'black',
@@ -12,6 +12,16 @@ module.exports = {
       workboxOptions: {
         swSrc: 'src/service-worker.js'
       },
+      performance: {
+        hints: 'warning',
+        // 入口起点的最大体积
+        maxEntrypointSize: 50000000,
+        // 生成文件的最大体积
+        maxAssetSize: 30000000,
+        // 只给出 js 文件的性能提示
+        assetFilter: function (assetFilename) {
+          return assetFilename.endsWith('.js')
+        },
       themeColor: '#728bf3'
     },
 }
